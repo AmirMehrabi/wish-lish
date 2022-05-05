0e2aa7e7f33f9b090eb6b2f50cff11d22f448b1f
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2022-05-05 23:54:49 +0430

    Changed a label for more clarity

--- resources/js/Pages/Add.vue
+++ resources/js/Pages/Add.vue
@@ -118,7 +118,7 @@
                                 class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
                                 for="inline-password"
                             >
-                                قیمت
+                                قیمت(تومان)
                             </label>
                         </div>
                         <div class="md:w-2/3">
@@ -126,7 +126,7 @@
                                 v-model="form.price"
                                 class="bg-gray-100 appearance-none border-2 border-gray-100 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                 id="inline-name"
-                                type="text"
+                                type="number"
                                 placeholder=""
                             />
                             <div v-if="errors.price" class="text-red-700">{{ errors.price }}</div>
